695a2139d151d5294e3ffa5a220d465be9bd43f8
Arun M <arun_m91@outlook.com>
2018-11-01 00:27:53 +0530

    removed stream name

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 '''Please add the stream name here'''
-CLOUDWATCH_STREAMNAME = ['/aws/rds/instance/prod-replica-enc/audit']
+CLOUDWATCH_STREAMNAME = ['']
 
 '''If new stream is added in the CLOUDWATCH_STREAM, Repective files should be created in the opt/cloudwatch-scripts/
  Directory and .pkl files should be touched !! '''
